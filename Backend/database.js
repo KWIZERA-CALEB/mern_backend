e74107f8b8f53ae6d5f538420c298c7ed79a291b
KWIZERA-CALEB <146452204+KWIZERA-CALEB@users.noreply.github.com>
2024-05-18 07:15:24 +0200

    Update database.js

--- Backend/database.js
+++ Backend/database.js
@@ -2,7 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const db_name = 'book_store'
-const db_url = `mongodb://127.0.0.1:27017/${db_name}`
+const db_url = `mongodb+srv://kwizeracaleb91:l0a0BWqmLeHXp26L@mernvercel.ezhejls.mongodb.net/${db_name}?retryWrites=true&w=majority&appName=mernvercel`
+
 
 const connect = function(cb) {
     mongoose.connect(db_url)
