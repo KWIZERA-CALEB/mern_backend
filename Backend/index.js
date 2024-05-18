3fd567c17723e38574379bac6ee8f575a8cf224c
KWIZERA-CALEB <146452204+KWIZERA-CALEB@users.noreply.github.com>
2024-05-18 07:11:15 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,6 +15,13 @@ app.use(bodyParser.json())
 
 app.use(cors())
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://mern-stack-deploy.vercel.app')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+})
 
 //configure to have access on uploads
 app.use('/uploads', express.static('uploads'))
